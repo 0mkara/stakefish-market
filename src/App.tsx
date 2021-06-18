7d0e43b60c4a73556791b59e49fc757cc89d97f9
0mkar <0mkar@protonmail.com>
2021-06-19 00:18:10 +0530

    design

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import useFetch from 'use-http';
+import { Container } from 'react-bootstrap';
 import './App.css';
 import { TableView } from './views/TableView';
 
 function App() {
   const { loading, error, data = [] } = useFetch('https://api.coingecko.com/api/v3/exchanges', {}, [])
   return (
-    <div className="App">
+    <Container className="App">
       {
         !(loading && error) && <TableView exchanges={data} />
       }
-    </div>
+    </Container>
   );
 }
 
